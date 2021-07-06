bd6b7509aedcbf2b2c57b6d55be0401c973490d6
Matias Ortiz <ortiz.matias@yahoo.com.ar>
2021-07-06 08:51:11 -0300

    background almostblack

---
+++ main.py
@@ -0,0 +1,11 @@
+#usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+__author__ = "Matias Ortiz"
+__email__ = "matias.ortiz@bvstv.com"
+__webpage__ = "https://github.com/mortiz-code"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2021, all rights reserved."
+__license__ = "BSD 3-Clause License."
+
+
